f065e7a9e487be9914476c3d36e3bd2b16ba6f5a
Akcel Arab <arab231@CAMPUS@V304V-NCP041.campus.unicaen.fr>
2026-02-02 15:47:19 +0100

    Nettoyage : remise au propre du portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -29,7 +29,7 @@ function Projects() {
               isBlog={false}
               title="CoreWar"
               description="Simulateur complet en Java avec machine virtuelle MARS, interface graphique et algorithme génétique pour générer automatiquement des programmes RedCode performants. Un projet mêlant système et IA."
-              ghLink="https://github.com/akcelarab/CoreWar" 
+              ghLink="https://github.com/akcel07/CoreWar" 
             />
           </Col>
 
@@ -39,7 +39,7 @@ function Projects() {
               isBlog={false}
               title="BlockWorld"
               description="Implémentation du 'Monde des Blocs' en Java. Exploration de l'IA via la modélisation, la planification et la résolution de contraintes pour la manipulation logique d'objets."
-              ghLink="https://github.com/akcelarab/BlockWorld"
+              ghLink="https://github.com/akcel07/BlockWorld"
             />
           </Col>
 
@@ -49,7 +49,7 @@ function Projects() {
               isBlog={false}
               title="BlackJack"
               description="Simulation de Blackjack en Java avec interface Swing. Utilisation de Design Patterns (Factory, Strategy) pour une architecture modulaire et une IA adverse performante."
-              ghLink="https://github.com/akcelarab/BlackJack"
+              ghLink="https://github.com/akcel07/BlackJack"
             />
           </Col>
 
@@ -59,7 +59,7 @@ function Projects() {
               isBlog={false}
               title="Bataille Navale"
               description="Jeu de stratégie reposant sur une architecture MVC. Inclut un mode console et une interface graphique Swing avec une gestion de grille optimisée."
-              ghLink="https://github.com/akcelarab/Bataille_Navale"
+              ghLink="https://github.com/akcel07/Bataille_Navale"
             />
           </Col>
 
@@ -69,7 +69,7 @@ function Projects() {
               isBlog={false}
               title="TodoList"
               description="Application desktop de gestion de tâches. Focus sur la persistance des données et la création d'une interface utilisateur intuitive pour le suivi d'objectifs."
-              ghLink="https://github.com/akcelarab/TodoList"
+              ghLink="https://github.com/akcel07/TodoList"
             />
           </Col>
 
